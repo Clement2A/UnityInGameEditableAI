8785a9e93604dfeebcb340ae3cfbbc0aa96655fd
Clement2A <44134904+Clement2A@users.noreply.github.com>
2024-06-04 14:50:24 +0200

    Added fleeing state

---
+++ Assets/Scripts/FSM/Behaviours/BehaviourFleePlayer.cs
@@ -0,0 +1,39 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FleeingState", menuName = "ScriptableObjects/Behaviour/FleePlayer")]
+public class BehaviourFleePlayer : Behaviour
+{
+    [SerializeField]
+    float distanceSquaredFromPlayer = 2;
+
+    Stats aiStats;
+    Transform playerTransform;
+
+
+    public override void DebugBehaviour()
+    {
+        Gizmos.color = Color.white;
+        Vector3 _playerPos = PlayerSystem.Instance.Player.transform.position;
+        Gizmos.DrawWireSphere(_playerPos, Mathf.Sqrt(distanceSquaredFromPlayer));
+        Gizmos.DrawLine(Owner.transform.position, _playerPos);
+    }
+
+    public override void OnEnter(FSM _owner)
+    {
+        aiStats = Owner.GetComponent<StatsComponent>().Stats;
+        playerTransform = PlayerSystem.Instance.Player.transform;
+    }
+
+    public override void OnExit()
+    {
+        //Do nothing
+    }
+
+    public override void OnUpdate()
+    {
+        Owner.transform.position = Vector3.MoveTowards(Owner.transform.position, playerTransform.position, -aiStats.speed * Time.deltaTime);
+    }
+}
